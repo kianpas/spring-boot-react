b7884754abb265a927ca326a88b304a675cbc4ab
kianpas <kianpas@gmail.com>
2021-12-02 23:50:01 +0900

    21.12.02 코스 추가

--- src/main/java/com/at/spring/course/CourseService.java
+++ src/main/java/com/at/spring/course/CourseService.java
@@ -17,38 +17,38 @@ public class CourseService {
 		courses.add(new Course(++idCounter, "in28minutes", "Master Microservices with Spring Boot and Spring Cloud"));
 		courses.add(new Course(++idCounter, "in28minutes",
 				"Deploy Spring Boot Microservices to Cloud with Docker and Kubernetes"));
+		courses.add(new Course(++idCounter, "in28minutes",
+				"Learn Full stack with Spring Boot and Vue"));
 	}
 
-	//전체 조회
+	// 전체 조회
 	public List<Course> findAll() {
 		return courses;
 	}
 
-	
-	//아이디 값으로 찾기
+	// 아이디 값으로 찾기
 	public Course findById(long id) {
 
-		//반복문으로 찾기
+		// 반복문으로 찾기
 		for (Course course : courses) {
 			if (course.getId() == id) {
 				return course;
 			}
 		}
-		
+
 		return null;
 	}
 
-	
-	//아이디 값으로 찾고 삭제
+	// 아이디 값으로 찾고 삭제
 	public Course deleteById(long id) {
 		Course course = findById(id);
 
-		//없을 경우 null
+		// 없을 경우 null
 		if (course == null) {
 			return null;
 		}
 
-		//삭제
+		// 삭제
 		if (courses.remove(course)) {
 			return course;
 		}
